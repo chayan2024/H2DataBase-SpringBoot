f10fce63466cce19dc42a5ce94bd0039b19d4d50
chayan <chayanchowdhury@gmail.com>
2024-05-20 19:42:58 +0530

    Initial commit

---
+++ src/main/java/org/example/h2databasespringboot/H2DataBaseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.h2databasespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DataBaseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DataBaseSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/h2databasespringboot/H2DataBaseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.h2databasespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DataBaseSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
